d7e2172c868256a2a1ecb1a7d3a7b17245cc4e80
DIVADING <2313211521@qq.com>
2021-10-18 18:51:16 +0800

    初始化

--- src/index.js
+++ src/index.js
@@ -5,13 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//将<React.StrictMode><App /></React.StrictMode>,  替换成了<App />,
 reportWebVitals();
